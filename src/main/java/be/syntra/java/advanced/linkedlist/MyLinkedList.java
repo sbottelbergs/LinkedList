f4844e00739d93eb2bfffb34a1fcb2e63dffd082
Steve Bottelbergs <steve.bottelbergs@continuum.be>
2019-11-28 20:40:27 +0100

    Add basic MyLinkedList implementation

---
+++ src/main/java/be/syntra/java/advanced/linkedlist/MyLinkedList.java
@@ -0,0 +1,48 @@
+package be.syntra.java.advanced.linkedlist;
+
+public class MyLinkedList<E> implements MyList<E> {
+    @Override
+    public void add(int i, E e) {
+
+    }
+
+    @Override
+    public void set(int i, E e) {
+
+    }
+
+    @Override
+    public E get(int i) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(E e) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
